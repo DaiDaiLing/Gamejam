36499a9c5f8780a1a66dc829e008e41b538610de
DESKTOP-6GB6O5C\林辛源 <4a9k0001@stust.edu.tw>
2021-06-12 16:03:02 +0800

    unity code

---
+++ Assets/code/MouseLook.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+
+    public Transform Body;
+
+    float xRotation = 0f;
+
+    //public GameObject lookat;
+    //public GameObject DefaultBlock;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        Body.Rotate(Vector3.up * mouseX);
+
+
+        Ray ray = new Ray(transform.position, transform.forward * 5.5f);
+        Debug.DrawLine(transform.position, transform.position + transform.forward * 5.5f, Color.red);
+        /*
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 5.5f, 1 << 0 | 1 << 8 | 1 << 12))
+        {
+            lookat = hit.collider.gameObject;
+            //Debug.LogWarning(hit.collider.gameObject);
+        }
+        else
+        {
+            lookat = DefaultBlock;
+        }
+        */
+    }
+}
---
+++ Assets/code/Tentacle.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tentacle : MonoBehaviour
+{
+    private LineRenderer lr;
+    private Vector3 grapplePoint;
+    public LayerMask canGrappleable;
+    public Transform TentacleTip, camera, player;
+    private float maxDistance = 100f;
+    private SpringJoint joint;
+    public Transform playerPosition;
+
+    void Awake()
+    {
+        lr = this.GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartGrapple();
+            
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            StopGrapple();
+        }
+        
+        if (Input.GetMouseButtonDown(0)&&Input.GetMouseButton(1))
+        {
+            HookForward();
+            Debug.Log(0);
+        }
+    }
+
+    void LateUpdate()
+    {
+        DrawRope();
+
+    }
+
+    void StartGrapple()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(camera.position, camera.forward, out hit, maxDistance, canGrappleable)) 
+        {
+            grapplePoint = hit.point;
+            joint = player.gameObject.AddComponent<SpringJoint>();
+            joint.autoConfigureConnectedAnchor = false;
+            joint.connectedAnchor = grapplePoint;
+
+            float distanceFromPoint = Vector3.Distance(player.position, grapplePoint);
+
+            joint.maxDistance = distanceFromPoint * 0.8f;
+            joint.minDistance = distanceFromPoint * 0.25f;
+
+            joint.spring = 4.5f;
+            joint.damper = 7f;
+            joint.massScale = 4.5f;
+
+            lr.positionCount = 2;
+            currentGrapplePosition = TentacleTip.position;
+
+            
+            
+        }
+
+    }
+
+    void StopGrapple()
+    {
+        lr.positionCount = 0;
+        Destroy(joint);
+    }
+
+    private Vector3 currentGrapplePosition;
+
+    void DrawRope()
+    {
+        if (!joint) return;
+
+        currentGrapplePosition = Vector3.Lerp(currentGrapplePosition, grapplePoint, Time.deltaTime * 8f);
+
+        lr.SetPosition(0, TentacleTip.position);
+        lr.SetPosition(1, currentGrapplePosition);
+    }
+
+    public bool IsGrappling()
+    {
+        return joint != null;
+    }
+
+    public Vector3 GetGrapplePoint()
+    {
+        return grapplePoint;
+    }
+    
+    void HookForward()
+    {
+        playerPosition.transform.position = Vector3.MoveTowards(playerPosition.position, grapplePoint, 10.0f * Time.deltaTime);
+    }
+}
